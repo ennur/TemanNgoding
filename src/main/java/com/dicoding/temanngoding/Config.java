44c58827e28c26a725280f218b04ac6152b86cc3
Nur Rohman <ennurr31@gmail.com>
2017-02-03 00:40:23 +0700

    update

--- src/main/java/com/dicoding/temanngoding/Config.java
+++ src/main/java/com/dicoding/temanngoding/Config.java
@@ -22,9 +22,9 @@ public class Config
     @Bean
     public DataSource getDataSource()
     {
-        String dbUrl=System.getenv("JDBC_DATABASE_URL");
-        String username=System.getenv("JDBC_DATABASE_USERNAME");
-        String password=System.getenv("JDBC_DATABASE_PASSWORD");
+        String dbUrl="postgres://rhrkrhgkrzdagp:98650e8fe3ef7613daca4edfd9bea361dffe73e7a4198a092ea279caaa9ada50@ec2-107-20-141-145.compute-1.amazonaws.com:5432/d5fc9iok388cqb";
+        String username="rhrkrhgkrzdagp";
+        String password="98650e8fe3ef7613daca4edfd9bea361dffe73e7a4198a092ea279caaa9ada50";
 
         DriverManagerDataSource ds=new DriverManagerDataSource();
         ds.setDriverClassName("org.postgresql.Driver");
